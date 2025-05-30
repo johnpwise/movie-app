32524cf9b107a4783c42cdd5a6ea4442a4f35a21
John <c413ab0d@opayq.com>
2025-05-30 09:09:53 +0100

    Cypress E2E added.

---
+++ cypress/e2e/dashboard-view-spec.cy.ts
@@ -0,0 +1,13 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('should display the dashboard view', () => {
+    // Act
+    cy.visit('/');
+    
+    // Assert
+    cy.get('.ma-dashboard-container').should('exist');
+  });
+});
